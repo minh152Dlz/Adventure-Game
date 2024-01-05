5767a6056f217f52feb659cbbd2101e4b6e38327
vynghiep <99947028+vynghiep@users.noreply.github.com>
2024-01-05 23:12:39 +0700

    background

--- Project/Adventure's Pipi/Assets/Scripts/Scene.cs
+++ Project/Adventure's Pipi/Assets/Scripts/Scene.cs
@@ -1,14 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement; 
+using UnityEngine.SceneManagement;
 
 public class Scene : MonoBehaviour
 {
-   private void OnTriggerEnter2D(Collider2D conllision){
-    if(conllision.CompareTag("Player")){
-        Scenecontroller.instance.NextLevel();
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // Kiểm tra xem đối tượng va chạm có tag là "Player" không
+        if (collision.CompareTag("Player"))
+        {
+            // Lấy ra Scenecontroller.instance một lần để tránh việc gọi hàm NextLevel trên một đối tượng null
+            Scenecontroller sceneController = Scenecontroller.instance;
 
-    }}
-    
+            if (sceneController != null)
+            {
+                // Gọi NextLevel chỉ khi sceneController không phải là null
+                sceneController.NextLevel();
+            }
+            else
+            {
+                Debug.LogWarning("Scenecontroller.instance is null. Make sure it is properly initialized.");
+            }
+        }
+    }
 }
