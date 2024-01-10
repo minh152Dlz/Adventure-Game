58e9d6047c330c9a24c81137d54309eed531c692
vynghiep <99947028+vynghiep@users.noreply.github.com>
2024-01-10 19:55:26 +0700

    map and ladder

--- Project/Adventure's Pipi/Assets/Scripts/backgroundcontroller.cs
+++ Project/Adventure's Pipi/Assets/Scripts/backgroundcontroller.cs
@@ -4,17 +4,20 @@ using UnityEngine;
 
 public class backgroundcontroller : MonoBehaviour
 {
-    [Range(-1f,1f)]
+    [Range(-1f, 1f)]
     public float scrollSpeed = 0.5f;
     private float offset;
     private Material mat;
 
-    void Start(){
+    void Start()
+    {
         mat = GetComponent<Renderer>().material;
-
+        mat.mainTexture.wrapMode = TextureWrapMode.Repeat; // Đặt wrap mode cho texture
     }
-    void Update(){
-        offset +=(Time.deltaTime * scrollSpeed)/10f;
-        mat.SetTextureOffset("_MainTex", new Vector2(offset,0));
+
+    void Update()
+    {
+        offset += (Time.deltaTime * scrollSpeed) / 10f;
+        mat.SetTextureOffset("_MainTex", new Vector2(offset, 0));
     }
 }
